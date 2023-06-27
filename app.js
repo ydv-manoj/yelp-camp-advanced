00289b5ba889d39ef22dc7a00150889d5cac624b
ydv-manoj <76946492+ydv-manoj@users.noreply.github.com>
2023-06-27 14:52:49 +0530

    Update app.js new

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 
 app.get("*", function(req, res) {
   res.render("error");
-});
+}); 
 
 app.listen( 3000, function() {
   console.log("listening on http://localhost:3000/");
